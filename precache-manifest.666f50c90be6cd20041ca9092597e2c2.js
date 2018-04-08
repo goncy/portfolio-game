cde0b2005861679c060766ad3b5f1faac8ad9f33
goncy <gonzalo.pozzo4@gmail.com>
2018-04-08 14:52:54 -0300

    Updates

---
+++ precache-manifest.666f50c90be6cd20041ca9092597e2c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-game/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "451d4f709798076b0feb",
+    "url": "/portfolio-game/js/app.451d4f70.js"
+  },
+  {
+    "revision": "9b6cfc068a9c9db475d8",
+    "url": "/portfolio-game/js/vendor.9b6cfc06.js"
+  },
+  {
+    "revision": "a87a17cebdd48f2802a4",
+    "url": "/portfolio-game/js/manifest.a87a17ce.js"
+  },
+  {
+    "revision": "a50b82aedc086f9b2ab377f8fffe1fa8",
+    "url": "/portfolio-game/index.html"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-game/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-game/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-game/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-game/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f59dab4bafcde16f61f16274c25a755e",
+    "url": "/portfolio-game/favicon.png"
+  },
+  {
+    "revision": "451d4f709798076b0feb",
+    "url": "/portfolio-game/css/app.06b4f236.css"
+  },
+  {
+    "revision": "65d45ebee021fb8d24971b25c3e0fdd9",
+    "url": "/portfolio-game/banner.png"
+  }
+];
